329e14d3fdf2142ec3266f246ff30907ac2cf615
Billy Bennett <bennett.billy@gmail.com>
2020-11-13 19:18:20 -0500

    end workshop

--- session-09/sketch.js
+++ session-09/sketch.js
@@ -8,142 +8,80 @@
 // based on animated events.
 
 
-let w = 200;
 let spirit;
+let scream, screamDur;
+let shatter;
 let glass;
-let scale = 0;
-let scream, screamDur, screamPlaying;
-let shatter, shatterDur, shatterPlaying;
+let scale = 0.001;
+let mix = 1.0;
 let reverb;
 let rot = 0;
-let clicked = false;
 
 
 function preload() {
   spirit = loadImage('assets/spirit.png');
-  glass = loadImage('assets/broken-glass.png');
+  glass = loadImage('assets/broken-glass.png')
   scream = loadSound('assets/scream-oof-mono.mp3');
   shatter = loadSound('assets/break-glass.mp3');
 }
 
-
 function setup() {
-  let can = createCanvas(600, 600);
+  createCanvas(600, 600);
   colorMode(HSL);
   imageMode(CENTER);
 
-  screamDur = scream.duration();
-  shatterDur = shatter.duration();
-  // The nf() function allows us to write floating point numbers to a 
-  // specific decimal place.
-  console.log("scream is " + nf(screamDur, 0, 2) + " seconds long");
-  console.log("shatter is " + nf(shatterDur, 0, 2) + " seconds long");
+  masterVolume(0.3);
 
-  // Here we'll add some reverb and connect it to our sound.
+  screamDur = scream.duration();
+  console.log("scream is " + nf(screamDur, 0, 2) + " seconds long");
+
   reverb = new p5.Reverb();
-  
-  // We'll disconnect the scream sound from the Master so 
-  // that we only hear it processed through the reverb.
+
   scream.disconnect();
+
   // 3 second reverb time, decayRate of 2%
   reverb.process(scream, 3, 2);
-  
+
+  scream.play();
 }
 
 function draw() {
   background(80, 255, 90);
 
-  //// For intial testing, we can map the scale to the mouseX.
-  // scale = constrain(map(mouseX, 0, width, 20, 220), 20, 220);
-
-  // We will have to check these a couple times, 
-  // so let's only call them once.
-  screamPlaying = scream.isPlaying();
-  shatterPlaying = shatter.isPlaying();
-
-  // Here we get the current time of the scream sound.  
   let screamCur = scream.currentTime();
-  // And the progress scaled from 0-1.
-  screamProg = screamCur / screamDur;
+  let screamProg = screamCur/screamDur; // value between 0.0 & 1.0
 
-  // Let's stop the scaling right before the oof in the scream sound
-  // The last boolean argument constrains the values within our bounds
-  if(screamPlaying) {
-    scale = map(screamCur, 0.0, screamDur - 0.5, 10.0, 400.0, true);
-    // console.log("image scale is = " + scale);
+  // If the sound is playing, scale the image to the progress
+  // and change the wet/dry mix of the reverb,
+  // Else leave scale alone and set mix to fully 'dry'
+  if (scream.isPlaying()) {
+    scale = map(screamProg, 0.0, 1.0, 10, 400);
+    mix = map(screamCur, 0.0, screamDur, 1.0, 0.0);
   } else {
     scale = scale;
+    mix = 0.0;
   }
-  
-  // We will map the reverb mix from full reverb mix (wet) 
-  // to no reverb (dry) as the spirit gets closer to us.  
-  let m = map(screamCur, 0.0, screamDur, 1.0, 0.0);
-  reverb.drywet(m);
+  reverb.drywet(mix);
 
-  //// Our spirit from The Legend of Korra with no animations
-  // image(spirit, width*0.5, height*0.5, scale, scale);
-
-  // Would be nice to rotate the spirit as it 'falls' so we can use
-  // translate() and rotate() inside of push() and pop(). 
-  push();
-  translate(width * 0.5, height * 0.5);
-  // Check here to see if we have clicked the page yet.
-  if (clicked) {
-    rotate(rot);
-    image(spirit, 0, 0, scale, scale);
-  } //else {
-  //   scale = scale;
-  //   image(spirit, 0, 0, scale, scale);
-  // }
-  pop();
-
-  // Once our the scream sound is almost done 
-  // aka our spirit is close enough...
-  if (screamProg > 0.92) {
-  // Draw the glass shattered image
-    image(glass, width * 0.45, height * 0.4, width * 0.45, height * 0.45);
-  // ... and stop the rotation.
-    rot = rot;
-  } else {
-  // Otherwise keep rotating the spirit image.
-    rot = frameCount * 0.15;
-  }
-  
-  // If the scream is playing & near the end
-  // & the shatter sound is not playing, then play shatter.
-  if (screamPlaying && screamProg > 0.92 && !shatterPlaying) {
+  if (screamProg > 0.94 && !shatter.isPlaying()) {
     shatter.play();
   }
 
-  //// We only want to play the first 1.5 seconds of the shatter sound, 
-  // but for some reason, the addCue() function is buggy.
-  
-  // shatter.addCue(1.5, shatterStop);
+  push();
+  translate(width * 0.5, height * 0.5);
+  rotate(rot);
+  image(spirit, 0, 0, scale, scale);
+  pop();
 
-  // So we can hand code it like so.  
-  let shatterCur = shatter.currentTime();
-  // console.log(shatterCur);
-  if (shatterCur > 1.5) {
+  if (screamProg < 0.94 && scream.isPlaying()) {
+    rot = frameCount*0.15;
+  } else {
+    rot = rot;
+    image(glass, width * 0.45, height *0.4, width * 0.45, height *0.4);
+  }
+
+  if (shatter.currentTime() > 1.5) {
     shatter.stop();
-    console.log("shatter stopped");
   }
-}
 
-function mousePressed() {
-  // The first time we click on the canvas 
-  // will start the spirit image rotating.
-  clicked = true;
-  if (!screamPlaying) {
-    scream.play();
-    console.log("scream playing");
-  } else if (screamPlaying) {
-    scream.stop();
-    console.log("scream stopped");
-  }
-}
-
-// This is a callback from the addCue() function which is buggy rn.
-function shatterStop() {
-  shatter.stop();
-  console.log('shatter stopped');
 }
